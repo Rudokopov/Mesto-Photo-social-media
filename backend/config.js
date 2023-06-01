daf0f34629138d36bf6c1acd3752008b1d28e3bd
Pavel Uzuniants <holabeda@gmail.com>
2023-06-01 18:19:51 +0300

    fix: Починил БД

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
-const { PORT = 3000 } = process.env
-DATA_BASE = process.env
+const { PORT = 3000, DATA_BASE } = process.env
 // const DATA_BASE = "mongodb://127.0.0.1:27017"
 
 export { PORT, DATA_BASE }
