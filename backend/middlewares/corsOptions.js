19a5155db2ee653c6b349efdf6a2c8296f869131
Pavel Uzuniants <holabeda@gmail.com>
2023-06-03 18:38:19 +0300

    fix: Починил критический баг который приводил к крашу приложения

--- backend/middlewares/corsOptions.js
+++ backend/middlewares/corsOptions.js
@@ -1,7 +1,5 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  "http://mesto-rudokopov.nomoredomains.monster",
-  "https://mesto-rudokopov.nomoredomains.monster",
   "https://mesto-rouge.vercel.app",
   "http://localhost:3000",
   "http://localhost:3001",
@@ -15,20 +13,3 @@ export const corsOptions = {
   optionsSuccessStatus: 200, // Установить статус успешного ответа для метода OPTIONS
   allowedHeaders: "*", // Разрешить все заголовки в запросе
 }
-
-// eslint-disable-next-line consistent-return
-// export const checkCORS = (req, res, next) => {
-//   const { method } = req
-//   const { origin } = req.headers
-//   const requestHeaders = req.headers["access-control-request-headers"]
-//   res.header("Access-Control-Allow-Credentials", true)
-//   if (allowedCors.includes(origin)) {
-//     res.header("Access-Control-Allow-Origin", origin)
-//   }
-//   if (method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS)
-//     res.header("Access-Control-Allow-Headers", requestHeaders)
-//     return res.end()
-//   }
-//   next()
-// }
