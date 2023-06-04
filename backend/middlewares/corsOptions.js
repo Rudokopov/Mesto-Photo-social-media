31b2176cd890f36d179ccaba29293737354172e5
Pavel Uzuniants <holabeda@gmail.com>
2023-06-04 13:40:20 +0300

    Обновил CORS политику, добавил homepage

--- backend/middlewares/corsOptions.js
+++ backend/middlewares/corsOptions.js
@@ -1,6 +1,6 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  "https://mesto-social.vercel.app/",
+  "https://rudokopov.github.io/Mesto-Photo-social-media",
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3002",
